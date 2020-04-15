7eb3a197352d9a18a4d0b833d8c1d0fabc6aefe5
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-04-15 10:49:35 -0400

    styled login page

--- client/src/components/LoginWindow/LoginWindow.js
+++ client/src/components/LoginWindow/LoginWindow.js
@@ -1,21 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LoginWindow.module.css";
 import { Container, Row, Col, Button } from "reactstrap";
+import classnames from "classnames";
 
 const LoginWindow = () => {
+  const [activeView, setActiveView] = useState("1");
+
+  const toggle = (id) => {
+    if (activeView !== id) {
+      setActiveView(id);
+    }
+    console.log(activeView);
+  };
+
   return (
     <Container className={styles.container} fluid={true}>
       <Row>
-        <Col className={styles.loginBox} sm="12" md={{ size: 6, offset: 3 }}>
-          <h1 className={styles.title}>Welcome back.</h1>
-          <form className={styles.form}>
-            <input className={styles.input} placeholder="Email" />
-            <input className={styles.input} placeholder="Password" />
-            <Button className={styles.loginBtn} outline color="success">
-              Login
-            </Button>
-          </form>
-        </Col>
+        {activeView === "1" ? (
+          <Col className={styles.loginBox} sm="12" md={{ size: 4, offset: 4 }}>
+            <Row>
+              <Col xs="6" sm="6" md="6" className={styles.titleCol}>
+                <h1 className={styles.title} onClick={() => toggle("1")}>
+                  Sign In
+                </h1>
+              </Col>
+              <Col xs="6" sm="6" md="6" className={styles.titleInactiveCol}>
+                <h1 className={styles.title} onClick={() => toggle("2")}>
+                  Sign Up
+                </h1>
+              </Col>
+            </Row>
+            <Row
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+                flex: 1,
+              }}
+            >
+              <Col
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <h2 className={styles.message}>Welcome back</h2>
+                <form className={styles.form}>
+                  <input className={styles.input} placeholder="Email" />
+                  <input className={styles.input} placeholder="Password" />
+                  <Button className={styles.loginBtn}>Login</Button>
+                </form>
+              </Col>
+            </Row>
+          </Col>
+        ) : (
+          <Col className={styles.loginBox} sm="12" md={{ size: 4, offset: 4 }}>
+            <Row>
+              <Col xs="6" sm="6" md="6" className={styles.titleInactiveCol}>
+                <h1 className={styles.title} onClick={() => toggle("1")}>
+                  Sign In
+                </h1>
+              </Col>
+              <Col xs="6" sm="6" md="6" className={styles.titleCol}>
+                <h1 className={styles.title} onClick={() => toggle("2")}>
+                  Sign Up
+                </h1>
+              </Col>
+            </Row>
+            <Row
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "stretch",
+                flex: 1,
+              }}
+            >
+              <Col
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <h2 className={styles.message}>Get started</h2>
+                <form className={styles.form}>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="First Name"
+                  />
+                  <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Last Name"
+                  />
+                  <input
+                    className={styles.input}
+                    type="email"
+                    placeholder="Email"
+                  />
+                  <input
+                    className={styles.input}
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <Button className={styles.loginBtn}>Sign Up</Button>
+                </form>
+              </Col>
+            </Row>
+          </Col>
+        )}
       </Row>
     </Container>
   );
