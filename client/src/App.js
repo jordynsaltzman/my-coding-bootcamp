faca58f5a06aa1a1a2e20cdedaf5098c48452bf3
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-07-10 15:01:28 -0400

    implemented private route for homepage if user is not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import HomePage from "./components/HomePage/HomePage";
 import AboutPage from "./components/AboutPage/AboutPage";
 import LoginWindow from "./components/LoginWindow/LoginWindow";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 const App = () => {
+  const token = localStorage.getItem("token");
   return (
     <Router>
       <Route exact path="/login" component={LoginWindow} />
-      <Route path="/home" component={HomePage} />
+
+      {token ? (
+        <Route path="/home" component={HomePage} />
+      ) : (
+        <Redirect to={{ pathname: "/login" }} />
+      )}
       <Route path="/about" component={AboutPage} />
     </Router>
   );
