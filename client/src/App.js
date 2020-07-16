f131c7214afbdc30cec96e3abb3ac22dd77ef88a
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-07-15 20:21:31 -0400

    debugging routing issue

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import AboutPage from "./components/AboutPage/AboutPage";
 import LoginWindow from "./components/LoginWindow/LoginWindow";
 import AuthRoutes from "./RouteGuards/AuthRoutes";
 import UnauthRoutes from "./RouteGuards/UnauthRoutes";
-import { BrowserRouter as Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Switch>
       <UnauthRoutes path="/login" component={LoginWindow} />
-      <Route exact path="/about" component={AboutPage} />
+      <Route path="/about" component={AboutPage} />
       <AuthRoutes path="/" component={HomePage} />
     </Switch>
   );
